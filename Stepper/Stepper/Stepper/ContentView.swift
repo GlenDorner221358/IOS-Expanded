2cfeaa811671a46e249c3396ba4928a728661db5
GlenDorner221358 <113541242+GlenDorner221358@users.noreply.github.com>
2024-07-26 10:49:53 +0200

    Initial commit part 2
    Added the basic filestructure for stepper

---
+++ Stepper/Stepper/Stepper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stepper
+//
+//  Created by student on 2024/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("We r walking")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stepper/Stepper/Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by student on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
